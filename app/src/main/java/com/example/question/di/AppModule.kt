3500a9b3744efb6ceb7a5cbd865b0ec330d3689d
Hossein <hhasnabadi@yahoo.com>
2024-08-27 22:24:15 +0330

    Retrofit

---
+++ app/src/main/java/com/example/question/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.example.question.di
+
+import com.example.question.network.QuestionApi
+import com.example.question.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideQuestion():QuestionApi{
+        return  Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(QuestionApi::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/example/question/network/QuestionApi.kt
@@ -0,0 +1,11 @@
+package com.example.question.network
+
+import com.example.question.model.Question
+import retrofit2.http.GET
+import javax.inject.Singleton
+
+@Singleton
+interface QuestionApi {
+    @GET("world.json")
+    suspend fun getAllQuestion():Question
+}
