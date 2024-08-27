c9167c936355ad26025574506e2230a127d93733
Hossein <hhasnabadi@yahoo.com>
2024-08-27 22:24:15 +0330

    Retrofit

---
+++ app/src/main/java/com/example/question/screen/QuestionViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.question.screens
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.question.data.DataOrException
+import com.example.question.model.QuestionItem
+import com.example.question.repository.QuestionRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class QuestionViewModel @Inject constructor(
+    private val repository: QuestionRepository
+): ViewModel() {
+    private val data :MutableState<DataOrException<ArrayList<QuestionItem>
+            ,Boolean,Exception>> = mutableStateOf(
+        DataOrException(null,true,Exception("")))
+    init {
+        getAllQuestion()
+
+    }
+
+    private fun getAllQuestion(){
+        viewModelScope.launch {
+            data.value.loading = true
+            data.value = repository.getAllQuestion()
+            if (data.value.data.toString().isNotEmpty())
+            {
+                data.value.loading = false
+            }
+
+        }
+    }
+
+
+
+}
